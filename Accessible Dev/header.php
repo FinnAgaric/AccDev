6ecae84dd14b49c28a356cf78b7406c2bd60182c
Finn <94475810+FinnAgaric@users.noreply.github.com>
2024-05-04 00:31:17 +0100

    Edits to styling. Added font link to header.

--- Accessible Dev/header.php
+++ Accessible Dev/header.php
@@ -6,7 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
+  <!-- Favicon -->
   <link rel="icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico">
+
+  <!-- Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 
   <title><?php bloginfo('name'); ?></title>
 
